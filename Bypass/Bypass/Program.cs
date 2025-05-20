6b65945439e7ac765035ffe2dae2efd797d1e1b6
sirjan <sirja@ZIZKA>
2025-05-20 10:01:34 +0200

    i'll be real idk what the fuck is this

---
+++ Bypass/Bypass/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+
+namespace Bypass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Runspace rs = RunspaceFactory.CreateRunspace();
+            rs.Open();
+            PowerShell ps = PowerShell.Create();
+            ps.Runspace = rs;
+            String cmd = "$ExecutionContext.SessionState.LanguageMode | Out-File -FilePath C:\\Tools\\test.txt";
+            ps.AddScript(cmd);
+            ps.Invoke();
+            rs.Close();
+        }
+    }
+}
