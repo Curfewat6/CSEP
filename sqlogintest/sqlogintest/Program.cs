24ff467f9f8af3c14d3f904f19eaa79d5c85664f
sirjan <sirja@ZIZKA>
2025-06-19 18:07:52 +0200

    test login

---
+++ sqlogintest/sqlogintest/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace sqlogintest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String sqlServer = "dc01.corp1.com";
+            String database = "master";
+
+            String conString = "Server = " + sqlServer + "; Database = " + database + "; Integrated Security = True;";
+            SqlConnection con = new SqlConnection(conString);
+            try
+            {
+                con.Open();
+                Console.WriteLine("Auth success!");
+            }
+            catch
+            {
+                Console.WriteLine("Auth failed");
+                Environment.Exit(0);
+            }
+            String querylogin = "SELECT SYSTEM_USER;";
+            SqlCommand command = new SqlCommand(querylogin, con);
+            SqlDataReader reader = command.ExecuteReader();
+            reader.Read();
+            Console.WriteLine("Logged in as: " + reader[0]);
+            reader.Close();
+
+            String querypublicrole = "SELECT IS_SRVROLEMEMBER('public');"; // You can replace it with other role like "sysadmin", or "db_owner"
+            command = new SqlCommand(querypublicrole, con);
+            reader = command.ExecuteReader();
+            reader.Read();
+            Int32 role = Int32.Parse(reader[0].ToString());
+            if (role == 1)
+            {
+                Console.WriteLine("Yes bro you are what ever you looking for");
+            }
+            else
+            {
+                Console.WriteLine("You are not what you are looking for :(");
+            }
+            reader.Close();
+
+            con.Close();
+        }
+    }
+}
