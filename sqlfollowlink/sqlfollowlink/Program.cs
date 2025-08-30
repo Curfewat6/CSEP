0973b6ec84637d5e251036b268e1434bcea2a214
sirjan <sirja@ZIZKA>
2025-08-30 04:08:21 +0200

    enum linked servers

---
+++ sqlfollowlink/sqlfollowlink/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace sqlfollowlink
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String sqlServer = "sql11"; // THIS IS THE HOSTNAME OF THE SQL SERVER YOU HAVE A FOOTHOLD IN
+            String database = "master";
+
+            String conString = "Server = " + sqlServer + "; Database = " + database + "; Integrated Security = True;";
+            SqlConnection con = new SqlConnection(conString);
+            try
+            {
+                con.Open();
+                Console.WriteLine("Auth success!");
+            }
+            catch
+            {
+                Console.WriteLine("Auth failed");
+                Environment.Exit(0);
+            }
+
+            String execCmd = "EXEC sp_linkedservers;";
+
+            SqlCommand command = new SqlCommand(execCmd, con);
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Console.WriteLine("Linked SQL server: " + reader[0]);
+            }
+            reader.Close();
+
+            con.Close();
+        }
+    }
+}
